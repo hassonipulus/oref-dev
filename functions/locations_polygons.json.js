953e6949f0d1d3a611b178107ac9198a3731e47b
tomer <tomer.konforty@arkhivist.io>
2026-03-17 19:03:33 +0200

    set cache time to 1 hour

--- functions/locations_polygons.json.js
+++ functions/locations_polygons.json.js
@@ -21,7 +21,7 @@ export async function onRequestGet(context) {
   const toCache = new Response(response.body, {
     headers: {
       'Content-Type': 'application/json',
-      'Cache-Control': 'public, max-age=60',
+      'Cache-Control': 'public, max-age=3600',
     },
   });
 
